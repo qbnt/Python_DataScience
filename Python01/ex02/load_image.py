21c243c4cb91822f1e34cf747cc50408f7c2f274
qbnt <quentin.banet@hotmail.fr>
2025-04-03 15:15:26 +0200

    Day01 - ex02

---
+++ Python01/ex02/load_image.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import numpy
+
+
+def ft_load(path: str) -> numpy.array:
+    try:
+        img = Image.open(path)
+    except FileNotFoundError as e:
+        print(f"File not found: {e}")
+        exit()
+    img = img.convert("RGB")
+    arr = numpy.array(img)
+    print(f"The shape of image is: {arr.shape}")
+
+    return arr
